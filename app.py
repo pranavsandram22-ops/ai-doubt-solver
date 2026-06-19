656ad0f3d8ed6c7be2b869d4707ba84e9fba4d19
pranavsandram22-ops <pranavsandram22@gmail.com>
2026-06-19 13:38:35 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def ask_gemini(prompt, image=None):
 
         try:
             genai.configure(api_key=key)
-            model = genai.GenerativeModel("gemini-1.5-flash")
+            model = genai.GenerativeModel("gemini-2.5-flash")
 
             if image is not None:
                 response = model.generate_content([prompt, image])
