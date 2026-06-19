d20884c28f279b70901df425b33827a143f4303f
pranavsandram22-ops <pranavsandram22@gmail.com>
2026-06-19 14:40:47 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -87,10 +87,11 @@ def ask_ai(prompt):
             return response.choices[0].message.content
 
         except Exception as e:
-                st.error(f"{model_name} failed")
-                st.exception(e)
 
-return "All models failed."
+            st.error(f"{model_name} failed")
+            st.exception(e)
+
+    return "All models failed."
 
 # Explain button
 if st.button("🚀 Explain"):
